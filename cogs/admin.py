55f3763b5fca56eb148098a774c501942b0a3257
Elijah <elijahj.burtner2@gmail.com>
2023-11-14 13:54:15 -0800

    -Update to Discord.py 2.0     -Convert Commands to Hybrid to work with Prefix and Slash commands     -Fix formatting of remind command     -Reformat Main File     -Prepare for new commands to be added     -Update cog setup and listeners     -Enable Logging
    -Move Token to .env File
    -Restructuring of Root files

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,65 +1,63 @@
-#---Imports---#
-import discord
-from discord.ext import commands
-from discord import Member
-from discord.ext.commands import has_permissions, MissingPermissions
+# ---Imports---#
 import json
 import os
-import re
 import time
-import asyncio
-import datetime
+import discord
+import discord.utils
+from discord import app_commands
+from discord.ext import commands
+from discord.ext.commands import has_permissions, Bot
+from dotenv import load_dotenv
 
 
-#---Set up Admin Class---#
+# ---Set up Admin Class---#
 class Admin(commands.Cog):
     def __init__(self, bot):
-        self.client = bot
+        self.bot = bot
 
     @commands.Cog.listener()
     async def on_ready(self):
         print("Admin Cog Loaded!")
         print("-----------------------------------------------")
 
-    #---Purge Messages---#
-    @commands.command()
+    # ---Purge Messages---#
+    @commands.hybrid_command()
     @has_permissions(administrator=True)
-    async def purge(self, ctx, pAmount):
-        ###Make pAmount an int, and add 1 to Delete Your Message Too###
-        pAmount = int(pAmount)
-        strpAmount = str(pAmount)
-        pAmount += 1
-        ###pAmount Needs to be Inbetween 1-99###
-        if pAmount > 101 or pAmount < 1:
+    async def purge(self, ctx, messagesamount):
+        ###Make messagesamount an int, and add 1 to Delete Your Message Too###
+        messagesamount = int(messagesamount)
+        strmessagesamount = str(messagesamount)
+        messagesamount += 1
+        ###messagesamount Needs to be In-between 1-99###
+        if messagesamount > 101 or messagesamount < 1:
             await ctx.send(
-                "Bad Arguments!\n ```Usage:\n  >purge(1,100)\n \n   Note: You can only delete up to 100 messages, that are no older than 14 days```"
+                "Bad Arguments!\n ```Usage:\n  >purge(1,100)\n \n   Note: You can only delete up to 100 messages, "
+                "that are no older than 14 days```"
             )
-        ###If Agruments are Correct, Run Purge, and Limit to pAmount###
+        ###If Arguments are Correct, Run Purge, and Limit to pAmount###
         else:
-            await ctx.send("Purging `" + strpAmount + "` Messages!")
-            time.sleep(3)
-            await ctx.message.delete()
-            time.sleep(0.2)
-            await ctx.channel.purge(limit=pAmount)
-            await ctx.send("Deleted `" + strpAmount + "` Messages!")
+            await ctx.send("Purging `" + strmessagesamount + "` Messages!")
+            time.sleep(1)
+            await ctx.channel.purge(limit=messagesamount)
+            await ctx.send("Deleted `" + strmessagesamount + "` Messages!")
 
-    #---Ban Timer---#
-    @commands.command()
+    # ---Ban Timer---#
+    @commands.hybrid_command()
     @has_permissions(administrator=True)
-    async def bantimer(self, ctx, member: discord.Member, banHour, banMin, *, banReason):
-        intbanHour = int(banHour)
-        intbanMin = int(banMin)
+    async def bantimer(self, ctx, member: discord.Member, banhour, banmin, *, banreason):
+        intbanHour = int(banhour)
+        intbanMin = int(banmin)
         ###Convert Hours and Minutes into Seconds for async sleep###
         hourtoSeconds = intbanHour * 3600
         mintoSeconds = intbanMin * 60
         finalSeconds = hourtoSeconds + mintoSeconds
         intfinalSeconds = int(finalSeconds)
 
-        if banReason == "":
-            banReason = "No reason given"
+        if banreason == "":
+            banreason = "No reason given"
         mentionUser = member.mention
-        await ctx.send(mentionUser + " will be banned for ```" + banHour + "Hr" + banMin + "Min```")
-        await member.ban(reason=banReason)
+        await ctx.send(mentionUser + " will be banned for ```" + banhour + "Hr" + banmin + "Min```")
+        await member.ban(reason=banreason)
         await asyncio.sleep(intfinalSeconds)
 
     @bantimer.error
@@ -67,14 +65,14 @@ class Admin(commands.Cog):
         await ctx.send(
             "Improper syntax or permissions! \n \n ```Usage: \n  >bantimer {user} {hours} {minutes} {reason}```")
 
-    #---Tempban---#
-    @commands.command()
+    # ---Temp-ban---#
+    @commands.hybrid_command()
     @has_permissions(administrator=True)
-    async def tempban(self, ctx, member: discord.Member, bHour, bMin, *, reason):
-        intbHour = int(bHour)
-        intbMin = int(bMin)
+    async def tempban(self, ctx, member: discord.Member, bhour, bmin, *, reason):
+        intbHour = int(bhour)
+        intbMin = int(bmin)
         serverName = member.guild.name
-        link = await ctx.channel.create_invite(max_age = 100000)
+        link = await ctx.channel.create_invite(max_age=100000)
         ###Convert Hours and Minutes into Seconds for async sleep###
         hourSeconds = intbHour * 3600
         minSeconds = intbMin * 60
@@ -84,20 +82,21 @@ class Admin(commands.Cog):
         if reason == "":
             reason = "No reason given"
         mentionUser = member.mention
-        await ctx.send(mentionUser + " is temporarily banned for ```" + bHour + "Hr" + bMin + "Min```")
-        await member.send("You have been banned from " + serverName + " for ```" + bHour + "Hr" + bMin + "Min```" + "Reason: " + reason + "\n When you are unbanned, click the link to join")
+        await ctx.send(mentionUser + " is temporarily banned for ```" + bhour + "Hr" + bmin + "Min```")
+        await member.send(
+            "You have been banned from " + serverName + " for ```" + bhour + "Hr" + bmin + "Min```" + "Reason: " + reason + "\n When you are unbanned, click the link to join")
         await member.send(link)
         await member.ban(reason=reason)
         await asyncio.sleep(intsecondsFinal)
         await ctx.guild.unban(member)
         await ctx.send(mentionUser + " Has been unbanned")
 
-
     @tempban.error
     async def tempban_error(error, ctx, self):
         await ctx.send(
             "Improper syntax or permissions! \n \n ```Usage: \n  >tempban {user} {hours} {minutes} {reason}```")
 
+
 ###Setup the cogs###
-def setup(bot):
-    bot.add_cog(Admin(bot))
+async def setup(bot):
+    await bot.add_cog(Admin(bot))
